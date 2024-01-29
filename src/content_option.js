06cf5b8148508f50ecff016e0372f7d530860006
DuyThong <duythong.ptit@gmail.com>
2024-01-29 18:18:31 +0700

    Init

---
+++ src/content_option.js
@@ -0,0 +1,153 @@
+const logotext = "SO MEDIA";
+const meta = {
+    title: "So Media",
+    description: "We are a photography club at PTIT. At Media Book, you can further develop your media and design skills as well as many interesting and dynamic activities.",
+};
+
+const introdata = {
+    title: "We are So Media ",
+    animated: {
+        first: "We love photography",
+        second: "We love videography",
+        third: "We love design",
+    },
+    description: "We are a photography club at PTIT. At So Media, you can further develop your media and design skills as well as many interesting and dynamic activities.",
+    your_img_url: "https://assets.unlayer.com/stock-templates/1706451135609-homepic.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+    img: "https://assets.unlayer.com/stock-templates/1706454526536-380272415_644128067824993_6241835109622914696_n.jpg",
+    description: "So Media TVC 2023 - The Growth",
+    link: "https://www.youtube.com/watch?v=ZrfJXRTsrEo",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706454838255-Screenshot%202024-01-28%20221342.png",
+        description: "Ngay dau tien PTIT Version ",
+        link: "https://www.youtube.com/watch?v=y7Y7VtKb9rY",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706517867378-Screenshot%202024-01-29%20154244.png",
+        description: "Profile Shooting 2023",
+        link: "https://www.facebook.com/somedia.vn.2016/videos/198116229592288/",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706518095715-399828778_669829441921522_1834242137200868664_n.jpg",
+        description: "TEDxHANU 2023",
+        link: "https://www.facebook.com/photo/?fbid=669829851921481&set=pcb.669830815254718",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706518226945-397551449_664880412416425_6495822736177543714_n.jpg",
+        description: "KACHOUFUUGETSU HANU",
+        link: "https://www.facebook.com/photo/?fbid=664880609083072&set=pcb.664904622414004",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706518396742-387207800_652564120314721_4877621676947583441_n.jpg",
+        description: "BREAK THE SHELL",
+        link: "https://www.facebook.com/photo?fbid=652563173648149&set=pcb.652571926980607",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706518644364-Screenshot%202024-01-29%20155655.png",
+        description: "DONG PHUC PTIT 2023",
+        link: "https://www.facebook.com/somedia.vn.2016/videos/305607272023242",
+    },
+    {
+        img: "https://assets.unlayer.com/stock-templates/1706518792689-345846649_261663956353151_7859952513778676032_n.jpg",
+        description: "INSIDE OUT",
+        link: "https://www.facebook.com/photo/?fbid=579806644257136&set=pcb.579806707590463",
+    },
+    // {
+    //     img: "https://assets.unlayer.com/stock-templates/1706518644364-Screenshot%202024-01-29%20155655.png",
+    //     description: "DONG PHUC PTIT 2023",
+    //     link: "https://www.facebook.com/somedia.vn.2016/videos/305607272023242",
+    // },
+    
+    
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "clbsomediaptit@gmail.com",
+    YOUR_FONE: "(+84) 39 7403 808",
+    description: "Let's do something interesting together!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_u6zo9qt",
+    YOUR_TEMPLATE_ID: "template_34mz99q",
+    YOUR_USER_ID: "uI2wlAs7frUBvbJkG",
+};
+
+const socialprofils = {
+    facebook: "https://www.facebook.com/somedia.vn.2016",
+    instagram: "https://www.instagram.com/so_media_2016/",
+    tiktok: "https://www.tiktok.com/@clbsomedia",
+    youtube:"https://www.youtube.com/@SoMedia2016"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/home/index.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Typewriter from "typewriter-effect";
+import { introdata, meta } from "../../content_option";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import home1 from "../../assets//images/home1.jpg";
+import home2 from "../../assets//images/home2.jpg";
+import home3 from "../../assets//images/home3.jpg";
+import home4 from "../../assets//images/home4.jpg";
+import home5 from "../../assets//images/home5.jpg";
+import home6 from "../../assets//images/home6.jpg";
+import ImageSlider from "./Imageslider";
+export const Home = () => {
+  const images1 = [home1, home2, home3];
+  const images = [home3, home4, home5];
+
+  const images2 = [home5, home6, home1];
+
+  
+  return (
+    <HelmetProvider>
+      <section id="home" className="home">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <div className="intro_sec d-block d-lg-flex align-items-center">
+          <div className="h_bg-image order-1 order-lg-2 h-100 home-image">
+            <Row style={{ paddingTop: "40px", marginTop: "0px", marginBottom: "0px" }}>
+              <Col>
+                <ImageSlider images={images} interval={5000} />
+              </Col>
+            </Row>
+            <Row style={{ paddingTop: "280px", marginTop: "0px", marginBottom: "0px" }}>
+              <Col>
+                <ImageSlider images={images1} interval={5000} />
+              </Col>
+            </Row>
+            <Row style={{ paddingTop: "280px", marginTop: "0px", marginBottom: "0px" }}>
+              <Col>
+                <ImageSlider images={images2} interval={5000} />
+              </Col>
+            </Row>
+          </div>
+
+          <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+            <div className="align-self-center ">
+              <div className="intro mx-auto">
+                <h2 className="mb-1x">{introdata.title}</h2>
+                <h1 className="fluidz-48 mb-1x" >
+                  <Typewriter
+                    options={{
+                      strings: [
+                        introdata.animated.first,
+                        introdata.animated.second,
+                        introdata.animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 10,
+                      
+                    }}
+                  />
+                </h1>
+                <p className="mb-1x">{introdata.description}</p>
+                <div className="intro_btn-action pb-5">
+                  <Link to="/portfolio" className="text_2">
+                    <div id="button_p" className="ac_btn btn ">
+                      Our Projects
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
+                  <Link to="/contact">
+                    <div id="button_h" className="ac_btn btn">
+                      Contact Us
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </HelmetProvider>
+  );
+};
