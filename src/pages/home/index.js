5fbc3c78382b518651f1cdc8f5649e75220251cf
DuyThong <duythong.ptit@gmail.com>
2024-01-31 11:51:41 +0700

    Add margin-top to text container and adjust font size for smaller screens

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -46,7 +46,7 @@ export const Home = () => {
             </Row>
           </div>
 
-          <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+          <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center" style={{ marginTop: "200px" }}>
             <div className="align-self-center ">
               <div className="intro mx-auto">
                 <h2 className="mb-1x">{introdata.title}</h2>
