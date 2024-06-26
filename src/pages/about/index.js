38bd67d0c24aa9d479a0d1a9af69566c2dbe68f3
DuyThong <duythong.ptit@gmail.com>
2024-06-26 09:39:53 +0700

    Update image carousel and activities timeline, and remove unnecessary CSS styles

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -48,26 +48,7 @@ export const About = () => {
             </div>
           </Col>
         </Row>
-        <Row className=" sec_sp">
-          <Col lg="4">
-            <h3 className="color_sec py-4">Achievements</h3>
-          </Col>
-          <Col lg="8">
-            <table className="table caption-top">
-              <tbody>
-                {worktimeline.map((data, i) => {
-                  return (
-                    <tr key={i}>
-                      <th scope="row">{data.jobtitle}</th>
-                      <td>{data.where}</td>
-                      <td>{data.date}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Col>
-        </Row>
+
         <Row className="sec_sp">
           <Col lang="4">
             <h3 className="color_sec py-4">4 Department</h3>
