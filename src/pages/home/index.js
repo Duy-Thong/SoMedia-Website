4666a176af7b567e3ab22ed6a110f5ebc8cf17d9
DuyThong <duythong.ptit@gmail.com>
2024-07-01 18:22:37 +0700

    chore: Update meta tags and fonts in index.html

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,21 +5,8 @@ import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
-import home1 from "../../assets//images/home1.jpg";
-import home2 from "../../assets//images/home2.jpg";
-import home3 from "../../assets//images/home3.jpg";
-import home4 from "../../assets//images/home4.jpg";
-import home5 from "../../assets//images/home5.jpg";
-import home6 from "../../assets//images/home6.jpg";
-import home7 from "../../assets//images/home7.jpg";
-import home8 from "../../assets//images/home8.jpg";
-import home9 from "../../assets//images/home9.jpg";
 import ImageSlider from "./Imageslider";
 export const Home = () => {
-  const images1 = [home1, home2, home5];
-  const images = [home4, home6, home7];
-  const images2 = [home3, home8, home9];
-
   return (
     <HelmetProvider>
       <section id="home" className="home">
