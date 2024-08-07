c6d2dcd0026f235a6bb5376b9cadcff1be13eaf3
Duy Thong <duythong.ptit@gmail.com>
2024-08-07 11:36:20 +0700

    chore: Update preloader styles for consistent display and improved visual presentation

--- src/components/preload/Pre.js
+++ src/components/preload/Pre.js
@@ -13,7 +13,14 @@ function Pre() {
     return () => clearTimeout(timer);
   }, []);
 
-  return <div id={load ? "preloader" : "preloader-none"}></div>;
+  return (
+    <div id={load ? "preloader" : "preloader-none"}>
+      <svg height="100" stroke="#FFFFFF" stroke-width="2" className="text-line" width="100%">
+        <text x="50%" dominant-baseline="middle" text-anchor="middle" y="50%">#So Media</text>
+      </svg>
+    </div>
+  );
+
 }
 
 export default Pre;
