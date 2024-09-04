5469b45b7ea1525b00197f6783d03fe00de90422
Duy Thong <duythong.ptit@gmail.com>
2024-09-04 21:56:16 +0700

    fix timeline image size

--- src/content_option.js
+++ src/content_option.js
@@ -315,10 +315,6 @@ const recruitmentpage =
     title1: "Being trained by seniors in skills in photography, videography, design, layout, thinking, color and software use. From there, you will gain the necessary skills for both study and future work.",
     title2: "Not only will you learn knowledge, you will also learn soft skills such as event organization, team management, teamwork,... through many events that the club organizes.",
     button: "Apply Now !",
-    cvtime: "From 15/10/2023 to 15/11/2023",
-    interviewtime: "From 20/11/2023 to 25/11/2023",
-    trainingtime: "From 01/12/2023 to 15/12/2023",
-    passtime: "From 20/12/2023 to 25/12/2023",
     applylink: "https://www.facebook.com/somedia.vn.2016",
 };
 // thông tin về các hoạt động của CLB theo từng tháng
